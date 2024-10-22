e0eabfd36048984411d430f6895f2474a8818fa3
Jonas Maziero <jonasmaziero@gmail.com>
2024-10-22 07:55:56 -0300

    20241022

--- notas_de_aula/fo_ohq.py
+++ notas_de_aula/fo_ohq.py
@@ -1,3 +1,3 @@
 def wf(alpha,u,n):
     from math import exp,sqrt,pi 
-    return sqrt(715)*alpha**(1/4)*u*(128*u**14 - 6720*u**12 + 131040*u**10 - 1201200*u**8 + 5405400*u**6 - 11351340*u**4 + 9459450*u**2 - 2027025)*exp(-u**2/2)/(21621600*pi**(1/4))
+    return alpha**(1/4)*exp(-u**2/2)/pi**(1/4)
