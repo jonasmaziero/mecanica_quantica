0470c933803f0d09fc52af411121d713a770f3c7
Jonas Maziero <jonasmaziero@gmail.com>
2025-04-16 16:51:28 -0300

    20250416

--- notas_de_aula/psiRp.py
+++ notas_de_aula/psiRp.py
@@ -2,7 +2,7 @@ import numpy as np
 import math 
 
 def psi_R(Z,mu,a0,x):
-    return sqrt(2)*sqrt(Z**3/a0**3)*(2 - x)*exp(-x/2)/4
+    return sqrt(6)*x*sqrt(Z**3/a0**3)*exp(-x/2)/12
 
 def sqrt(x):
     return np.sqrt(x) 
